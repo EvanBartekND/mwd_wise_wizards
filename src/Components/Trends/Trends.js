01ee729a4e2d7026ad51853190e9e8f54ff528e7
Evan Bartek <ebartek@student05.cse.nd.edu>
2025-10-15 21:54:45 -0400

    front end skeleton done

---
+++ src/Components/Trends/Trends.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+export default function Trends() {
+  return (    
+    <>
+        <h1>Trends Page</h1>
+        <h2>Input the date range you wish to see</h2>
+        <input /> <button>submit</button>
+    </>
+)
+};
+
